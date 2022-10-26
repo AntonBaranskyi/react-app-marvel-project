145be241764ce4ce997b57212302d5de3cce11c6
Anton <antonbaranskij12@gmail.com>
2022-10-26 23:40:18 +0300

    Work with charList component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -2,78 +2,62 @@ import Services from '../../servises/data';
 import './charList.scss';
 import abyss from '../../resources/img/abyss.jpg';
 import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import styled from 'styled-components';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class CharList extends Component {
 
     state = {
-        name:null,
-        thumbnail:null
-    }   
-    componentDidMount(){
-        this.getDataHeroes();
+        heroes : [],
+        loading:true,
+        error:false
     }
-    heroService = new Services();
-    getDataHeroes = ()=>{
-        this.heroService.getAllHeroes()
-        .then(resp=>{
-           let neededAnsw = resp.slice(0,9);
-           console.log(neededAnsw);
-            neededAnsw.map(item=>{
-                this.setState({
-                    name:item.name,
-                    thumbnail:item.thumbnail
-                })
-            })
+    componentDidMount(){
+        this.getHeroesData();
+    }
+    newService = new Services();
+
+    onError = ()=>{
+        this.setState({
+            error:true
         })
     }
+
+    getHeroesData = () =>{
+        this.newService.getAllHeroes().then(resp=>{
+            this.setState({
+                heroes:resp.slice(0,9),
+                loading:!this.state.loading
+            })
+        }).catch(this.onError)
+    }
+
     render(){
-        const {name,thumbnail} = this.state;
-    return (
-        <div className="char__list">
+        const {heroes,loading,error} = this.state;
+        const loadingImg = loading ? <Spinner/> : null
+        const errorImg = error ? <ErrorMessage/>:null
+        return(
+            <div className="char__list">
             <ul className="char__grid">
-                <li className="char__item">
+                {loadingImg}
+                {errorImg}
+                {heroes.map(({name,thumbnail})=>{
+                    return(
+                        <li className="char__item">
                     <img src={thumbnail} alt="abyss"/>
                     <div className="char__name">{name}</div>
                 </li>
-                <li className="char__item char__item_selected">
-                    <img src={thumbnail} alt="abyss"/>
-                    <div className="char__name">{name}</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
+                    )
+                })}
+            
             </ul>
             <button className="button button__main button__long">
                 <div className="inner">load more</div>
             </button>
         </div>
-    )
-}
-}
+        )
+    }
 
+}
 export default CharList;
