72e87787d625b09cc6c5e28ca9638be2356eb5c4
Anton <antonbaranskij12@gmail.com>
2022-10-26 21:14:51 +0300

    Work with list of heroes

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,13 +1,39 @@
+import Services from '../../servises/data';
 import './charList.scss';
 import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
 
-const CharList = () => {
+class CharList extends Component {
+
+    state = {
+        name:null,
+        thumbnail:null
+    }   
+    componentDidMount(){
+        this.getDataHeroes();
+    }
+    heroService = new Services();
+    getDataHeroes = ()=>{
+        this.heroService.getAllHeroes()
+        .then(resp=>{
+           let neededAnsw = resp.slice(0,9);
+           console.log(neededAnsw);
+            neededAnsw.map(item=>{
+                this.setState({
+                    name:item.name,
+                    photo:item.thumbnail.path + '.' + item.thumbnail.extension
+                })
+            })
+        })
+    }
+    render(){
+        const {name,thumbnail} = this.state;
     return (
         <div className="char__list">
             <ul className="char__grid">
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
+                    <img src={thumbnail} alt="abyss"/>
+                    <div className="char__name">{name}</div>
                 </li>
                 <li className="char__item char__item_selected">
                     <img src={abyss} alt="abyss"/>
@@ -47,6 +73,7 @@ const CharList = () => {
             </button>
         </div>
     )
+}
 }
 
 export default CharList;
