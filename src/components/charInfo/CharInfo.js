70dc87ac96926f4d4b16c512978c5b191c58e34f
Anton <antonbaranskij12@gmail.com>
2022-11-01 12:18:34 +0200

    Add propTypes validation to the project

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -4,102 +4,107 @@ import Services from "../../servises/data";
 import Skeleton from "../skeleton/Skeleton";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
+import PropTypes from "prop-types";
 
 class CharInfo extends Component {
-    state = {
-        hero: null,
-        loading: false,
-        error: false,
-    };
-    componentDidMount() {
-        this.onHeroDataUpdate();
-    }
+  state = {
+    hero: null,
+    loading: false,
+    error: false,
+  };
+  componentDidMount() {
+    this.onHeroDataUpdate();
+  }
 
-    componentDidUpdate(prevProps) {
-        // Типове використання (не забудьте порівняти пропси):
-        if (this.props.heroId !== prevProps.heroId) {
-            this.onHeroDataUpdate();
-        }
+  componentDidUpdate(prevProps) {
+    // Типове використання (не забудьте порівняти пропси):
+    if (this.props.heroId !== prevProps.heroId) {
+      this.onHeroDataUpdate();
     }
+  }
 
-    marvelService = new Services();
-    onHeroDataUpdate = () => {
-        const { heroId } = this.props;
-        if (!heroId) {
-            return;
-        }
-        this.onLoading();
-        this.marvelService
-            .getHero(heroId)
-            .then(this.updateHero)
-            .catch(this.onError);
-    };
-    updateHero = (hero) => {
-        console.log(hero);
-        this.setState({
-            hero,
-            loading: false,
-        });
-    };
-    onLoading = () => {
-        this.setState({
-            loading: true,
-        });
-    };
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true,
-        });
-    };
-    render() {
-        const { hero, loading, error } = this.state;
-
-        return (
-            <div className="char__info">
-                {!(hero || loading || error) && <Skeleton />}
-                {loading && <Spinner />}
-                {error && <ErrorMessage />}
-                {!(loading || error || !hero) && <View hero={hero} />}
-            </div>
-        );
+  marvelService = new Services();
+  onHeroDataUpdate = () => {
+    const { heroId } = this.props;
+    if (!heroId) {
+      return;
     }
+    this.onLoading();
+    this.marvelService
+      .getHero(heroId)
+      .then(this.updateHero)
+      .catch(this.onError);
+  };
+  updateHero = (hero) => {
+    console.log(hero);
+    this.setState({
+      hero,
+      loading: false,
+    });
+  };
+  onLoading = () => {
+    this.setState({
+      loading: true,
+    });
+  };
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+  render() {
+    const { hero, loading, error } = this.state;
+
+    return (
+      <div className="char__info">
+        {!(hero || loading || error) && <Skeleton />}
+        {loading && <Spinner />}
+        {error && <ErrorMessage />}
+        {!(loading || error || !hero) && <View hero={hero} />}
+      </div>
+    );
+  }
 }
 
 const View = ({ hero }) => {
-    let i = 0;
-    const { name, thumbnail, description, homepage, wiki, comics } = hero;
-    return (
-        <>
-            <div className="char__basics">
-                <img src={thumbnail} alt="abyss" />
-                <div>
-                    <div className="char__info-name">{name}</div>
-                    <div className="char__btns">
-                        <a href={homepage} className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href={wiki} className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div className="char__descr">{description}</div>
-            <div className="char__comics">Comics:</div>
-            <ul className="char__comics-list">
-                {comics.length == 0 ? 'Could not find any comics for this hero' : null}
-                {comics &&
-                    comics.map(({ name }) => {
-                        return (
-                            <li className="char__comics-item" key={i++}>
-                                {name}
-                            </li>
-                        );
-                    })}
-            </ul>
-        </>
-    );
+  let i = 0;
+  const { name, thumbnail, description, homepage, wiki, comics } = hero;
+  return (
+    <>
+      <div className="char__basics">
+        <img src={thumbnail} alt="abyss" />
+        <div>
+          <div className="char__info-name">{name}</div>
+          <div className="char__btns">
+            <a href={homepage} className="button button__main">
+              <div className="inner">homepage</div>
+            </a>
+            <a href={wiki} className="button button__secondary">
+              <div className="inner">Wiki</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="char__descr">{description}</div>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">
+        {comics.length == 0 ? "Could not find any comics for this hero" : null}
+        {comics &&
+          comics.map(({ name }) => {
+            return (
+              <li className="char__comics-item" key={i++}>
+                {name}
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
+};
+
+CharInfo.propTypes = {
+  heroId: PropTypes.number,
 };
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,96 +1,104 @@
-import Services from '../../servises/data';
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
-import { Component } from 'react';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
+import Services from "../../servises/data";
+import "./charList.scss";
+import abyss from "../../resources/img/abyss.jpg";
+import { Component } from "react";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
 class CharList extends Component {
+  state = {
+    heroes: [],
+    loading: true,
+    error: false,
+    offset: 219,
+    extraLoading: false,
+    heroEnded: false,
+  };
+  componentDidMount() {
+    this.getHeroesData();
+  }
+  newService = new Services();
 
-    state = {
-        heroes : [],
-        loading:true,
-        error:false,
-        offset:219,
-        extraLoading : false,
-        heroEnded : false
-    }
-    componentDidMount(){
-        this.getHeroesData();
-    }
-    newService = new Services();
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
+  };
 
-    onError = ()=>{
-        this.setState({
-            error:true,
-            loading:false
-        })
-    }
+  getHeroesData = () => {
+    this.newService
+      .getAllHeroes()
+      .then(this.onHeroesLoaded)
+      .catch(this.onError);
+  };
 
-    getHeroesData = () =>{
-        this.newService.getAllHeroes()
-        .then(this.onHeroesLoaded)
-        .catch(this.onError)
+  onHeroesLoaded = (heroes) => {
+    this.setState({
+      heroes,
+      loading: false,
+    });
+  };
+  onRequestMore = (offset) => {
+    this.onExtraLoading();
+    this.newService
+      .getAllHeroes(offset)
+      .then(this.onMoreHeroesLoaded)
+      .catch(this.onError);
+  };
+  onExtraLoading = () => {
+    this.setState({
+      extraLoading: true,
+    });
+  };
+  onMoreHeroesLoaded = (newHeroes) => {
+    let ended = false;
+    if (newHeroes.length < 9) {
+      ended = true;
     }
+    this.setState(({ heroes, offset }) => ({
+      heroes: [...heroes, ...newHeroes],
+      extraLoading: false,
+      offset: offset + 9,
+      heroEnded: ended,
+    }));
+  };
 
-    onHeroesLoaded = (heroes)=>{
-        this.setState({
-            heroes,
-            loading:false
-        })
-    }
-    onRequestMore = (offset)=>{
-        this.onExtraLoading()
-        this.newService.getAllHeroes(offset)
-        .then(this.onMoreHeroesLoaded)
-        .catch(this.onError)
-    }
-    onExtraLoading = ()=>{
-        this.setState({
-            extraLoading: true
-        })
-    }
-    onMoreHeroesLoaded = (newHeroes)=>{
-        let ended = false;
-        if(newHeroes.length < 9){
-            ended = true;
-        }
-        this.setState(({heroes,offset})=>({
-            heroes : [...heroes, ...newHeroes],
-            extraLoading : false,
-            offset: offset + 9,
-            heroEnded : ended
-        }))
-    }
-
-
-    render(){
-        const {heroes,loading,error,offset,heroEnded} = this.state;
-        const loadingImg = loading ? <Spinner/> : null
-        const errorImg = error ? <ErrorMessage/>:null
-        return(
-            <div className="char__list">
-            <ul className="char__grid">
-                {loadingImg}
-                {errorImg}
-                {heroes.map(({name,thumbnail,id})=>{
-                    return(
-                        <li className="char__item"
-                        key = {id}
-                        onClick = {()=> this.props.onGetHeroId(id)}>
-                    <img src={thumbnail} alt="abyss"/>
-                    <div className="char__name">{name}</div>
-                </li>
-                    )
-                })}
-            
-            </ul>
-            <button style={{display: heroEnded ? 'none' : 'block' }} onClick={()=>this.onRequestMore(offset)} className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-        )
-    }
-
+  render() {
+    const { heroes, loading, error, offset, heroEnded } = this.state;
+    const loadingImg = loading ? <Spinner /> : null;
+    const errorImg = error ? <ErrorMessage /> : null;
+    return (
+      <div className="char__list">
+        <ul className="char__grid">
+          {loadingImg}
+          {errorImg}
+          {heroes.map(({ name, thumbnail, id }) => {
+            return (
+              <li
+                className="char__item"
+                key={id}
+                onClick={() => this.props.onGetHeroId(id)}
+              >
+                <img src={thumbnail} alt="abyss" />
+                <div className="char__name">{name}</div>
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          style={{ display: heroEnded ? "none" : "block" }}
+          onClick={() => this.onRequestMore(offset)}
+          className="button button__main button__long"
+        >
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
 }
+
+CharList.propTypes = {
+  onGetHeroId: PropTypes.func,
+};
 export default CharList;
